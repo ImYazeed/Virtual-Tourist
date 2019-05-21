466dff3d4d0442e3af9d233b314313d5f0e2641c
ImYazeed <imYazeedALZahrani@gmail.com>
2019-05-21 21:54:40 +0300

    setup AlbumVC

---
+++ Virtual-Tourist/Controllers/AlbumViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AlbumViewController.swift
+//  Virtual-Tourist
+//
+//  Created by Yazeedo on 21/05/2019.
+//  Copyright © 2019 Yazeedo. All rights reserved.
+//
+
+import UIKit
+
+class AlbumViewController: UIViewController {
+
+    var selectedPin: Pin!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
