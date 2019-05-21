1ae4dcf281b7394033790cc6afe5cd8049e0c488
ImYazeed <imYazeedALZahrani@gmail.com>
2019-05-21 06:08:09 +0300

    Setup Core Data Controller + Core Data Configuration

---
+++ Virtual-Tourist/Model/CoreData/DataController.swift
@@ -0,0 +1,50 @@
+//
+//  DataController.swift
+//  Virtual-Tourist
+//
+//  Created by Yazeedo on 21/05/2019.
+//  Copyright © 2019 Yazeedo. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    static let shared = DataController()
+    
+    let persistentContainer: NSPersistentContainer
+    
+    var viewContext:NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    private init() {
+        persistentContainer = NSPersistentContainer(name: "Virtual_Tourist")
+    }
+    
+    func load(completion: (() -> Void)? = nil) {
+        persistentContainer.loadPersistentStores { storeDescription, error in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            self.autoSaveViewContext()
+            completion?()
+        }
+    }
+}
+
+extension DataController {
+    func autoSaveViewContext(interval:TimeInterval = 30) {
+        guard interval > 0 else {
+            return
+        }
+        saveContext()
+        DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
+            self.autoSaveViewContext(interval: interval)
+        }
+    }
+    func saveContext() {
+        if viewContext.hasChanges {
+            try? viewContext.save()
+        }
+    }
+}
